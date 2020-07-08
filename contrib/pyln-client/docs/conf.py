3c38e76333ffe7e94129f3000db7f1647d540bb4
Christian Decker <decker.christian@gmail.com>
2020-07-08 20:46:19 +0200

    fixup! pyln: Add stubs to generate documentation for pyln-client

--- contrib/pyln-client/docs/conf.py
+++ contrib/pyln-client/docs/conf.py
@@ -71,7 +71,7 @@ language = None
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-# The name of the Pygments (syntax highlighting) style to use.
+# The name of the Pygments style to use.
 pygments_style = None
 
 
